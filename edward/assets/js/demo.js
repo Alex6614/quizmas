5cbcfae8fcc5bcfd8c66ae825a17f0ac7a4e781a
Edward Hui <hkm727145@gmail.com>
2018-07-08 04:33:59 +0800

    Alter different style

--- edward/assets/js/demo.js
+++ edward/assets/js/demo.js
@@ -187,8 +187,8 @@ demo = {
         Chartist.Pie('#chartPreferences', dataPreferences, optionsPreferences);
 
         Chartist.Pie('#chartPreferences', {
-            labels: ['53%', '36%', '11%'],
-            series: [53, 36, 11]
+            labels: ['66%', '30%', '7%'],
+            series: [66, 30, 7]
         });
 
 
@@ -201,26 +201,6 @@ demo = {
             ]
         };
 
-        // var optionsSales = {
-        //   lineSmooth: false,
-        //   low: 0,
-        //   high: 800,
-        //    chartPadding: 0,
-        //   showArea: true,
-        //   height: "245px",
-        //   axisX: {
-        //     showGrid: false,
-        //   },
-        //   axisY: {
-        //     showGrid: false,
-        //   },
-        //   lineSmooth: Chartist.Interpolation.simple({
-        //     divisor: 6
-        //   }),
-        //   showLine: false,
-        //   showPoint: true,
-        //   fullWidth: true
-        // };
         var optionsSales = {
             lineSmooth: false,
             low: 0,
@@ -250,13 +230,12 @@ demo = {
 
         var chartHours = Chartist.Line('#chartHours', dataSales, optionsSales, responsiveSales);
 
-        // lbd.startAnimationForLineChart(chartHours);
-
         var data = {
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep'],
             series: [
-                [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-                [412, 243, 280, 580, 453, 353, 300, 364, 368, 410, 636, 695]
+                [200 , 232, 257, 240, 399, 453, 478, 512, 568],
+                [189 , 289, 238, 399, 410, 550, 580, 650, 850],
+                [89 , 139, 338, 423, 513, 713, 835, 899, 905]
             ]
         };
 
@@ -280,224 +259,6 @@ demo = {
         ];
 
         var chartActivity = Chartist.Bar('#chartActivity', data, options, responsiveOptions);
-
-        // lbd.startAnimationForBarChart(chartActivity);
-
-        // /* ----------==========     Daily Sales Chart initialization    ==========---------- */
-        //
-        // dataDailySalesChart = {
-        //     labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-        //     series: [
-        //         [12, 17, 7, 17, 23, 18, 38]
-        //     ]
-        // };
-        //
-        // optionsDailySalesChart = {
-        //     lineSmooth: Chartist.Interpolation.cardinal({
-        //         tension: 0
-        //     }),
-        //     low: 0,
-        //     high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-        //     chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
-        // }
-        //
-        // var dailySalesChart = Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
-
-        // lbd.startAnimationForLineChart(dailySalesChart);
-
-        //
-        //
-        // /* ----------==========     Completed Tasks Chart initialization    ==========---------- */
-        //
-        // dataCompletedTasksChart = {
-        //     labels: ['12am', '3pm', '6pm', '9pm', '12pm', '3am', '6am', '9am'],
-        //     series: [
-        //         [230, 750, 450, 300, 280, 240, 200, 190]
-        //     ]
-        // };
-        //
-        // optionsCompletedTasksChart = {
-        //     lineSmooth: Chartist.Interpolation.cardinal({
-        //         tension: 0
-        //     }),
-        //     low: 0,
-        //     high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-        //     chartPadding: { top: 0, right: 0, bottom: 0, left: 0}
-        // }
-        //
-        // var completedTasksChart = new Chartist.Line('#completedTasksChart', dataCompletedTasksChart, optionsCompletedTasksChart);
-        //
-        // // start animation for the Completed Tasks Chart - Line Chart
-        // lbd.startAnimationForLineChart(completedTasksChart);
-        //
-        //
-        // /* ----------==========     Emails Subscription Chart initialization    ==========---------- */
-        //
-        // var dataEmailsSubscriptionChart = {
-        //   labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        //   series: [
-        //     [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
-        //
-        //   ]
-        // };
-        // var optionsEmailsSubscriptionChart = {
-        //     axisX: {
-        //         showGrid: false
-        //     },
-        //     low: 0,
-        //     high: 1000,
-        //     chartPadding: { top: 0, right: 5, bottom: 0, left: 0}
-        // };
-        // var responsiveOptions = [
-        //   ['screen and (max-width: 640px)', {
-        //     seriesBarDistance: 5,
-        //     axisX: {
-        //       labelInterpolationFnc: function (value) {
-        //         return value[0];
-        //       }
-        //     }
-        //   }]
-        // ];
-        // var emailsSubscriptionChart = Chartist.Bar('#emailsSubscriptionChart', dataEmailsSubscriptionChart, optionsEmailsSubscriptionChart, responsiveOptions);
-        //
-        // //start animation for the Emails Subscription Chart
-        // lbd.startAnimationForBarChart(emailsSubscriptionChart);
-
-    },
-
-    initGoogleMaps: function() {
-        var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
-        var mapOptions = {
-            zoom: 13,
-            center: myLatlng,
-            scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
-            styles: [{
-                "featureType": "water",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#e9e9e9"
-                }, {
-                    "lightness": 17
-                }]
-            }, {
-                "featureType": "landscape",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#f5f5f5"
-                }, {
-                    "lightness": 20
-                }]
-            }, {
-                "featureType": "road.highway",
-                "elementType": "geometry.fill",
-                "stylers": [{
-                    "color": "#ffffff"
-                }, {
-                    "lightness": 17
-                }]
-            }, {
-                "featureType": "road.highway",
-                "elementType": "geometry.stroke",
-                "stylers": [{
-                    "color": "#ffffff"
-                }, {
-                    "lightness": 29
-                }, {
-                    "weight": 0.2
-                }]
-            }, {
-                "featureType": "road.arterial",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#ffffff"
-                }, {
-                    "lightness": 18
-                }]
-            }, {
-                "featureType": "road.local",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#ffffff"
-                }, {
-                    "lightness": 16
-                }]
-            }, {
-                "featureType": "poi",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#f5f5f5"
-                }, {
-                    "lightness": 21
-                }]
-            }, {
-                "featureType": "poi.park",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#dedede"
-                }, {
-                    "lightness": 21
-                }]
-            }, {
-                "elementType": "labels.text.stroke",
-                "stylers": [{
-                    "visibility": "on"
-                }, {
-                    "color": "#ffffff"
-                }, {
-                    "lightness": 16
-                }]
-            }, {
-                "elementType": "labels.text.fill",
-                "stylers": [{
-                    "saturation": 36
-                }, {
-                    "color": "#333333"
-                }, {
-                    "lightness": 40
-                }]
-            }, {
-                "elementType": "labels.icon",
-                "stylers": [{
-                    "visibility": "off"
-                }]
-            }, {
-                "featureType": "transit",
-                "elementType": "geometry",
-                "stylers": [{
-                    "color": "#f2f2f2"
-                }, {
-                    "lightness": 19
-                }]
-            }, {
-                "featureType": "administrative",
-                "elementType": "geometry.fill",
-                "stylers": [{
-                    "color": "#fefefe"
-                }, {
-                    "lightness": 20
-                }]
-            }, {
-                "featureType": "administrative",
-                "elementType": "geometry.stroke",
-                "stylers": [{
-                    "color": "#fefefe"
-                }, {
-                    "lightness": 17
-                }, {
-                    "weight": 1.2
-                }]
-            }]
-        };
-
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            title: "Hello World!"
-        });
-
-        // To add the marker to the map, call setMap();
-        marker.setMap(map);
     },
 
     showNotification: function(from, align) {
